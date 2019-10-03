0dfc11196d562fc7d775aafb3c25496d8538c3b9
Ayush Srivastava <34351218+AyushSri19-777@users.noreply.github.com>
2019-10-03 21:56:58 +0530

    Create prct.py

---
+++ prct.py
@@ -0,0 +1,12 @@
+a=[1,3,2,4,4,5]
+b=[2,3,5,6]
+c={2,3,5}
+print(set(a))
+a=set(a)
+b=set(b)
+print(a.union(b))
+print(a.intersection(b))
+print(a.issuperset(b))
+print(c.issubset(b))
+print(a.difference_update(b))
+print(a)
